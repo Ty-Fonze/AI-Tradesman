cc0b03256d668a90816d3dc87e45110c19176cb2
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 19:06:47 -0400

    Create app.jsx

---
+++ src/dashboard/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Widget from './components/Widget';
+import './styles/theme.css';
+
+const App = () => {
+    return (
+        <div>
+            <Widget />
+        </div>
+    );
+};
+
+export default App;
