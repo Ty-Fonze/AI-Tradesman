119454d8a06ffe5daf9c77ea711a77602d975a66
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 07:32:01 -0400

    Update preload.js

--- src/dashboard/preload.js
+++ src/dashboard/preload.js
@@ -1,8 +1,6 @@
 const { contextBridge } = require('electron');
-const path = require('path');
 
 // Expose an API to the renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
-  require: (module) => require(module),
-  pathJoin: (...args) => path.join(...args),
+  require: (module) => require(module), // Exposes `require` to the renderer
 });
