2c71eb226f74cbb80e6c7b809d58b600fabf6e38
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 07:26:50 -0400

    Update renderer.js

--- src/dashboard/renderer.js
+++ src/dashboard/renderer.js
@@ -1,6 +1,5 @@
 const { renderLiveTicker } = require('./widgets/live_ticker');
 
-// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     const appContainer = document.getElementById('app');
 
