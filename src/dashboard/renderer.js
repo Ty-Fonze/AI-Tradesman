69a4c791e7c98f1d97383581fc3fb61f7e5c2111
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 07:04:02 -0400

    Create renderer.js

---
+++ src/dashboard/renderer.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const appTitle = document.getElementById('app-title');
+  appTitle.textContent = "Welcome to the Warhammer-Themed Trading Dashboard!";
+});
