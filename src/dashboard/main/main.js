e11d0724ef388a58cd9d28cd70ab8d80f7156b0d
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 18:15:30 -0400

    Create main.js

---
+++ src/dashboard/main/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadFile('renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
