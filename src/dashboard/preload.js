24482def4387549f577051d4f741f17fa65239b0
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 07:44:33 -0400

    Update preload.js

--- src/dashboard/preload.js
+++ src/dashboard/preload.js
@@ -1,6 +1,10 @@
 const { contextBridge } = require('electron');
+const path = require('path');
 
-// Expose 'require' to the renderer process
+// Load the live_ticker module
+const liveTicker = require(path.join(__dirname, 'widgets/live_ticker'));
+
+// Expose the liveTicker functionality to the renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
-    require: (module) => require(module),
+    renderLiveTicker: liveTicker.renderLiveTicker,
 });
