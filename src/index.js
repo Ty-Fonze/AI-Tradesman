f642ff87c42d4149235cd74d380fc1fac52652f1
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 21:01:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use ReactDOM from 'react-dom/client'
-import App from './dashboard/App'; // Adjust this path if necessary
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
-root.render(<App />); // Use the new render method
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
