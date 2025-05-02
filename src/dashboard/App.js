8f2c1c3871b31ff19a2c105cc3c8a7a095ccee92
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 21:04:46 -0400

    Update App.js

--- src/dashboard/App.js
+++ src/dashboard/App.js
@@ -1,13 +1,12 @@
- import React from 'react';
- import Widget from './components/LiveTickerWidget';
- 
- const App = () => {
-     return (
-         <div>
-             <Widget />
-             <h3>Test App</h3>
-         </div>
-     );
- };
- 
- export default App;
+import React from 'react';
+import Widget from './components/LiveTickerWidget';
+
+const App = () => {
+    return (
+        <div>
+            <Widget />
+        </div>
+    );
+};
+
+export default App;
