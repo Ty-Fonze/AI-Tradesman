f2014ed209b9d6680bc94e9a457327d65def986c
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 20:13:35 -0400

    Update index.jsx

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './dashboard/App'; // Adjust path based on your App.jsx location
 
 ReactDOM.render(<App />, document.getElementById('root'));
