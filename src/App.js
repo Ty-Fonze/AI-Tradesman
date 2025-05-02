458fc8f019030584ab7aa62fe4f64b2857a6b11a
Ty-Fonze <72chevymalibue@gmail.com>
2025-05-01 21:46:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LiveTickerWidget from './components/LiveTickerWidget';
 
 const App = () => {
     return (
         <div>
             <h1>Hello, React!</h1>
+            <LiveTickerWidget />
         </div>
     );
 };
